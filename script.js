52a1c2614de7c763fd2b872bda4eb18d303f5735
Nafissa Hassan <nafissa_92@hotmail.com>
2017-08-14 19:43:53 +0000

    Finish Project7-wikiViewer

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 /*global $*/
+function adjustTitle(title){
+    var newTitle="";
+    for (var i=0; i<title.length; i++){
+        if (title[i]===" "){
+            title[i]="_";
+        }
+        newTitle+=title[i];
+    }
+        
+        return newTitle;
+    };
 $(document).ready(function(){
     $("#searchWikipedia").submit(function(event){
         event.preventDefault();
@@ -17,7 +28,8 @@ $(document).ready(function(){
                 var html="<ul class='list-group'>"
                 var results=response.query.search;
                 $.each(results, function(index,item){
-                    html+="<li class='list-group-item'><h2><a href='#'>"+item.title+"</a></h2></li><li class='list-group-item'><em>"+item.snippet+"</em></li>";    
+                    var searchTitle=
+                    html+="<li class='list-group-item'><h2><a href='https://en.wikipedia.org/wiki/"+adjustTitle(item.title)+"'>"+item.title+"</a></h2></li><li class='list-group-item'><em>"+item.snippet+"</em></li>";    
                 });
                 html+="</ul>"
                 $("#search-results").html(html);
